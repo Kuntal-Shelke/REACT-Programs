ca9e15962b88455fc51ee66ef3d673ec409aebdc
Kuntal-Shelke <sunitashelke567@gmail.com>
2024-07-20 10:15:11 +0530

    use Pure JavaScript into JSX

---
+++ Program2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+  let name="kuntal"
+  let a=10,b=20;
+  return (
+    <>
+      <p>This is JavaScript Expresion</p>
+      <p>Name is {name}</p>
+      <p> Addition is {a+b}</p>
+    </>
+  )
+}
+
+export default App

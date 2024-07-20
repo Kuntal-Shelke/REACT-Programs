728b70406f22d7052f6a8da46e1fc9c84e01d6d2
Kuntal-Shelke <sunitashelke567@gmail.com>
2024-07-20 10:00:58 +0530

    This is Simple button

---
+++ Program1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+       <h1>This is REACT Button</h1>
+       <button>Click Me </button>
+    </div>
+  )
+}
+
+export default App
